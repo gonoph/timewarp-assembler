5237a6ea18624b933c13fc9a033f2ef88a8235d2
Billy Holmes <billy@gonoph.net>
2018-01-15 17:43:19 -0500

    added sleep to keep cpu low. added segfault fence to show it never returns from _drives_insane

--- tw.s
+++ tw.s
@@ -1,5 +1,5 @@
 # This assembler code is Copyright (C) 2009 Billy Holmes at gonoph.net
-# Copyright (C) 2010,2018 (fixed a logic bug, and to run in newer gcc)
+# Copyright (C) 2010,2018 (fixed a logic bug, and to run in newer gas) - without gcc
 # Released and Licensed under GPLv2
 # http://www.gonoph.net/gpl-2.0.txt
 # Lyrics upon which it is based by Richard O'Brien and Richard Hartley
@@ -7,9 +7,13 @@
 # Coded in gnu assembler
 # Why? Couldn't resist
 
-.global main
+.section .rodata
+.align 4
+coredump:
+   .long 0xdeadbeaf
+
 .section .data
-.set _right, 0x00000001
+.align 4
 .set _hands, 0x00000002
 .set _knees, 0xffffffff
 
@@ -18,14 +22,15 @@ hips:
 tight:
    .long 0x0
 
+sleep:
+   .long 1,0
+
 .section .text
-   .align 4
+.align 4
+.global _start
 
-# it's just a:
-
-main:
+_start:
    jmp timewarp
-
 timewarp:
    jmp left
 step:
@@ -36,6 +41,7 @@ step:
    push (tight)
    call _pelvic_thrust
    call _drives_insane
+   movl $0, (coredump)     # fence to cause segfault of assignment to read only section - it'll never get here
 left:
    xor %eax, %eax
    inc %eax
@@ -51,4 +57,9 @@ _drives_insane:
    push $again
    ret
 again:
+   # call sys_nanosleep() and sleep for 1 second
+   mov     $sleep, %ebx
+   xor     %ecx, %ecx
+   mov     $0xa2, %eax
+   int     $0x80
    jmp timewarp
